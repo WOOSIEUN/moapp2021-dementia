a0e6f0da7071ab072f9f57604ac6d15659abf752
WOOSIEUN <ramtk6726@naver.com>
2021-05-25 22:28:40 +0900

    Initial Commit

---
+++ moapp2021-dementia/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  moapp2021-dementia
+//
+//  Created by EUN on 2021/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
